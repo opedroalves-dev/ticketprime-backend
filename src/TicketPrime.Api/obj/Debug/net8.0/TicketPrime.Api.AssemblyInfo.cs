b3ef3b0786b19bbf852fd582c1ceda361b844b45
Pedro <opedro.h.alves@gmail.com>
2026-06-04 20:31:24 -0300

    refactor: conclude phase 2 architecture migration

--- src/TicketPrime.Api/obj/Debug/net8.0/TicketPrime.Api.AssemblyInfo.cs
+++ src/TicketPrime.Api/obj/Debug/net8.0/TicketPrime.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TicketPrime.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d35df3801641a8d64bc289902130bfb43c56eaf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3938367f644efe2c9f7bac566510912e7453abca")]
 [assembly: System.Reflection.AssemblyProductAttribute("TicketPrime.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TicketPrime.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
